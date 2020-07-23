89829b1a517c2769d49caa5d230a5d66607cc09c
Brice Le Gallo <lebowvsky@gmail.com>
2020-07-23 02:44:52 +0200

    some routes done and Login

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(
 
 // DIVERS
 app.get("/api/divers", (req, res) => {
-  connection.query("SELECT * FROM diver", (err, result) => {
+  connection.query("SELECT * FROM diver", (err, results) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
@@ -26,16 +26,59 @@ app.get("/api/divers", (req, res) => {
   });
 });
 
+app.post("/api/divers", (req, res) => {
+  const formData = req.body;
+  connection.query("INSERT INTO diver SET ?", formData, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err.message);
+    } else {
+      res.sendStatus(200);
+    }
+  });
+});
+
 app.get("/api/divers/:idDiver", (req, res) => {
   const idDiver = req.params.idDiver;
   connection.query(
-    "SELECT * FROM diver WHERE id = ?",
+    "SELECT * FROM diver WHERE id_diver = ?",
     idDiver,
     (err, result) => {
       if (err) {
         res.status(500).send(err.message);
       } else {
-        res.json(results);
+        res.json(result);
+      }
+    }
+  );
+});
+
+app.get("/api/divers/redux/:login", (req, res) => {
+  const login = req.params.login;
+  connection.query(
+    "SELECT id_diver, lastname, firstname, email FROM diver WHERE email = ?",
+    login,
+    (err, result) => {
+      if (err || !result[0]) {
+        res.status(500).send('login inexistant');
+      } else {
+        res.json(result);
+      }
+    }
+  );
+});
+
+// LOGIN
+app.post("/api/divers/login", (req, res) => {
+  const { email, password } = req.body;
+  connection.query(
+    "SELECT * FROM diver WHERE email = ? AND password = ?",
+    [email, password],
+    (err, result) => {
+      if (err || !result[0]) {
+        res.status(500).send("Login ou Mot de passe erroné...");
+      } else {
+        res.send(result);
       }
     }
   );
@@ -47,7 +90,7 @@ app.get("/api/diving", (req, res) => {
     if (err) {
       res.status(500).send(err.message);
     } else {
-      res.json(results);
+      res(result.data);
     }
   });
 });
