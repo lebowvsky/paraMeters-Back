01063b934a914bf281671ec1079b31f9f91451e6
Brice Le Gallo <lebowvsky@gmail.com>
2020-07-27 21:46:19 +0200

    Passport and bcrypt Done

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const connection = require("./conf");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
 const app = express();
 const cors = require("cors");
 const port = 8080;
 
-app.use(cors());
+const verifyPass = (userPassword, hashedPassword) => {
+  return bcrypt.compareSync(userPassword, hashedPassword);
+};
 
+app.use(cors());
 // Support JSON-encoded bodies
 app.use(express.json());
 // Support URL-encoded bodies
@@ -15,6 +21,29 @@ app.use(
   })
 );
 
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+      session: false,
+    },
+    function (email, password, cb) {
+      connection.query(
+        "SELECT * FROM diver WHERE email = ?",
+        email,
+        (err, user) => {
+          if (err) return cb(err);
+          if (!user[0]) return cb(null, false, { error: "Login erroné" });
+          if (!verifyPass(password, user[0].password))
+            return cb(null, false, { error: "Mot de passe erroné" });
+          return cb(null, user[0]);
+        }
+      );
+    }
+  )
+);
+
 // DIVERS
 app.get("/api/divers", (req, res) => {
   connection.query("SELECT * FROM diver", (err, results) => {
@@ -22,18 +51,6 @@ app.get("/api/divers", (req, res) => {
       res.status(500).send(err.message);
     } else {
       res.json(results);
-    }
-  });
-});
-
-app.post("/api/divers", (req, res) => {
-  const formData = req.body;
-  connection.query("INSERT INTO diver SET ?", formData, (err, results) => {
-    if (err) {
-      console.log(err);
-      res.status(500).send(err.message);
-    } else {
-      res.sendStatus(200);
     }
   });
 });
@@ -68,20 +85,30 @@ app.get("/api/divers/redux/:login", (req, res) => {
   );
 });
 
-// LOGIN
-app.post("/api/divers/login", (req, res) => {
-  const { email, password } = req.body;
+// REGISTER
+app.post("/api/divers/register", (req, res) => {
+  const hashPass = bcrypt.hashSync(req.body.password, 10);
   connection.query(
-    "SELECT * FROM diver WHERE email = ? AND password = ?",
-    [email, password],
-    (err, result) => {
-      if (err || !result[0]) {
-        res.status(500).send("Login ou Mot de passe erroné...");
+    "INSERT INTO diver SET ?",
+    { ...req.body, password: hashPass },
+    (err, results) => {
+      if (err) {
+        console.log(err);
+        res.status(500).send(err.message);
       } else {
-        res.send(result);
+        res.sendStatus(200);
       }
     }
   );
+});
+
+// LOGIN
+app.post("/api/divers/login", (req, res) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) return res.status(500).send(err);
+    if (!user) return res.status(401).json(info);
+    return res.status(200).json({ user });
+  })(req, res);
 });
 
 // DIVING
@@ -157,7 +184,7 @@ app.post("/api/belong-to", (req, res) => {
 app.get("/api/divers/:idDiver/dives", (req, res) => {
   const idDiver = req.params.idDiver;
   connection.query(
-    "SELECT dr.id_diver, bg.diver_id, bg.diving_id,  DATE_FORMAT(dg.date_diving, '%d/%m/%Y') AS date, dg.hour_diving, dg.deep, dg.duration, dg.gas, dg.localisation FROM diver AS dr JOIN belong_to AS bg ON dr.id_diver=bg.diver_id JOIN diving AS dg ON dg.id_diving=bg.diving_id WHERE id_diver = ? ",
+    "SELECT dr.id_diver, bg.diver_id, bg.diving_id,  DATE_FORMAT(dg.date_diving, '%d/%m/%Y') AS date, dg.hour_diving, dg.deep, dg.duration, dg.gas, dg.localisation FROM diver AS dr JOIN belong_to AS bg ON dr.id_diver=bg.diver_id JOIN diving AS dg ON dg.id_diving=bg.diving_id WHERE id_diver = ? ORDER BY Date DESC",
     idDiver,
     (err, result) => {
       if (err) {
